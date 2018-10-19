edbe642d031150ae1ee6fed50ba4e77d26f27d1d
Ricardo Trujillo <rtrujillo@b1gdigital.com>
2018-10-18 21:35:16 -0500

    Created Main activity with binding layout

--- app/src/main/java/com/rijatru/development/nativappstest/MainActivity.java
+++ app/src/main/java/com/rijatru/development/nativappstest/MainActivity.java
@@ -1,4 +1,20 @@
 package com.rijatru.development.nativappstest;
 
-public class MainActivity {
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rijatru.development.nativappstest.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
 }
